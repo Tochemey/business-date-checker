86abe4f88befeca090fee9dce870cb6195957650
Tochemey <tochemey@hotmail.com>
2019-06-02 01:31:16 +0000

    Complete the implementation

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-#Business Date Checker
+# Business Date Library
+
+This libary helps validate business days, weekend days, week days and holidays for various countries in the world. So far 144 countries are supported.
+
+The validation and parsing of date are done without timezone and dates without times.
+
+# Features
+
+- Checking whether a date is a business date in a given country. _Business date is a date that is not a holiday nor a weekend day_
+- Calculation of the number of week days between two dates. _Week days Monday to Friday_
+- Calculation of the number of weekend days between two dates. _Weekend days Saturday and Sunday_
+- Calculation of the number of holidays between two dates in a given country
+- Checking whether a given date is a week day.
+- Checking whether a given date in a country is a holiday
+- Checking whether a given date is a weekend day.
+- Addition of a number of business days to a given date in a country to get the next business date.
+
+# References
+
+- [Worldwide holidays library](https://github.com/commenthol/date-holidays)
+- [Utilities for week days in Moment](https://github.com/jamesplease/moment-business)
---
+++ src/business.date.checker.spec.ts
@@ -0,0 +1,166 @@
+import { BusinessDateChecker } from './business.date.checker';
+
+describe('BusinessDateChecker', () => {
+  describe('December 25 2018 in the US', () => {
+    it('is not a business date', () => {
+      const date: Date = new Date('December 25 2018');
+      const country: string = 'US';
+      const valid = BusinessDateChecker.isBusinessDate(date, country);
+      expect(valid).toBeFalsy();
+    });
+  });
+
+  describe('December 26 2018 in the US', () => {
+    it('is not a business date', () => {
+      const date: Date = new Date('December 26 2018');
+      const country: string = 'US';
+      const output = BusinessDateChecker.isBusinessDate(date, country);
+      expect(output).toBeFalsy();
+    });
+  });
+
+  describe('January 22 2019 in the US', () => {
+    it('is not a business date', () => {
+      const date: Date = new Date('January 22 2019');
+      const country: string = 'US';
+      const output = BusinessDateChecker.isBusinessDate(date, country);
+      expect(output).toBeFalsy();
+    });
+  });
+
+  describe('January 23 2019 in the US', () => {
+    it('is a business date', () => {
+      const date: Date = new Date('January 23 2019');
+      const country: string = 'US';
+      const output = BusinessDateChecker.isBusinessDate(date, country);
+      expect(output).toBeTruthy();
+    });
+  });
+
+  describe('July 14 2019 in France', () => {
+    it('is a holiday', () => {
+      const date: Date = new Date('July 14 2019');
+      const country: string = 'FR';
+      const output = BusinessDateChecker.isHoliday(date, country);
+      expect(output).toBeTruthy();
+    });
+  });
+
+  describe('Holiday - January 22 2019 in the US', () => {
+    it('is a holiday', () => {
+      const date: Date = new Date('January 22 2019');
+      const country: string = 'US';
+      const output = BusinessDateChecker.isHoliday(date, country);
+      expect(output).toBeTruthy();
+    });
+  });
+
+  describe('June 2 2019', () => {
+    it('is a weekend day', () => {
+      const date: Date = new Date('June 2 2019');
+      const output = BusinessDateChecker.isWeekendDay(date);
+      expect(output).toBeTruthy();
+    });
+  });
+
+  describe('June 1 2019', () => {
+    it('is a weekend day', () => {
+      const date: Date = new Date('June 1 2019');
+      const output = BusinessDateChecker.isWeekendDay(date);
+      expect(output).toBeTruthy();
+    });
+  });
+
+  describe('June 5 2019', () => {
+    it('is a week day', () => {
+      const date: Date = new Date('June 5 2019');
+      const output = BusinessDateChecker.isWeekDay(date);
+      expect(output).toBeTruthy();
+    });
+  });
+
+  describe('Add 20 business days to December 25 2018', () => {
+    it('should return a valid business date of Friday, January 25th 2019', () => {
+      const date: Date = new Date('December 25 2018');
+      const delay: number = 20;
+      const country: string = 'US';
+      const result = BusinessDateChecker.addBusinessDays(date, delay, country);
+      expect(result).toEqual('Friday, January 25th 2019');
+    });
+  });
+
+  describe('Total Holidays between December 25th 2018 and January 25th 2019 in the US', () => {
+    it('should be 3', () => {
+      const date1: Date = new Date('December 25 2018');
+      const date2: Date = new Date('January 25 2019');
+      const country: string = 'US';
+      const result = BusinessDateChecker.holidaysBetween(date1, date2, country);
+      expect(result).toEqual(3);
+    });
+  });
+
+  describe('Total Weekend days between December 25th 2018 and January 25th 2019 in the US', () => {
+    it('should be 8', () => {
+      const date1: Date = new Date('December 25 2018');
+      const date2: Date = new Date('January 25 2019');
+      const result = BusinessDateChecker.weekendDaysBetween(date1, date2);
+      expect(result).toEqual(8);
+    });
+  });
+
+  describe('Holiday with invalid country code', () => {
+    it('should return false', () => {
+      const date: Date = new Date('January 22 2019');
+      const country: string = 'USAT';
+      const output = BusinessDateChecker.isHoliday(date, country);
+      expect(output).toBeFalsy();
+    });
+  });
+
+  describe('Total Week days between June 1 2019 and June 7th 2019 in the US', () => {
+    it('should be 4', () => {
+      const date1: Date = new Date('June 1 2019');
+      const date2: Date = new Date('June 7 2019');
+      const result = BusinessDateChecker.weekDaysBetween(date1, date2);
+      expect(result).toEqual(4);
+    });
+  });
+
+  describe('Business Date Checking with invalid country', () => {
+    it('should be false', () => {
+      const date: Date = new Date('January 23 2019');
+      const country: string = 'USAT';
+      const output = BusinessDateChecker.isBusinessDate(date, country);
+      expect(output).toBeFalsy();
+    });
+  });
+
+  describe('Holidays between January 25th 2019 and December 25th 2018 in the US', () => {
+    it('should return -1', () => {
+      const date1: Date = new Date('December 25 2018');
+      const date2: Date = new Date('January 25 2019');
+      const country: string = 'US';
+      const result = BusinessDateChecker.holidaysBetween(date2, date1, country);
+      expect(result).toEqual(-1);
+    });
+  });
+
+  describe('Add negative business days to December 25 2018', () => {
+    it('should be undefined', () => {
+      const date: Date = new Date('December 25 2018');
+      const delay: number = -1;
+      const country: string = 'US';
+      const result = BusinessDateChecker.addBusinessDays(date, delay, country);
+      expect(result).toBeUndefined();
+    });
+  });
+
+  describe('July 14 2019 in Benin', () => {
+    it('is not a holiday', () => {
+      const date: Date = new Date('July 14 2019');
+      const country: string = 'BJ';
+      const output = BusinessDateChecker.isHoliday(date, country);
+      expect(output).toBeFalsy();
+    });
+  });
+});
